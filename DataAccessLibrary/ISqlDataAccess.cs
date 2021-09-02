b657ed53d3a96d781b7bafe09ebccfa5e8c50b3a
C5315422 <pawel.szelag@sap.com>
2021-09-02 23:53:33 +0200

    add comments

--- DataAccessLibrary/ISqlDataAccess.cs
+++ DataAccessLibrary/ISqlDataAccess.cs
@@ -6,7 +6,22 @@ namespace DataAccessLibrary
     public interface ISqlDataAccess
     {
         string ConnectionStringName { get; set; }
+        /// <summary>
+        /// Method for loading data from DB
+        /// </summary>
+        /// <typeparam name="T">Type of model</typeparam>
+        /// <typeparam name="U">Type for params</typeparam>
+        /// <param name="sql">SQL query</param>
+        /// <param name="parameters">Params</param>
+        /// <returns></returns>
         Task<List<T>> LoadData<T, U>(string sql, U parameters);
+        /// <summary>
+        /// Method for saving data to DB
+        /// </summary>
+        /// <typeparam name="T">Type of model</typeparam>
+        /// <param name="sql">SQL query</param>
+        /// <param name="parameters">Params</param>
+        /// <returns></returns>
         Task SaveData<T>(string sql, T parameters);
     }
 }
